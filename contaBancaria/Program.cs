48c125ff57acadc8948b693fbcd3315a99876490
venicode <vitoria_aparecida2011@hotmail.com>
2023-12-14 16:10:40 -0300

    Adicionar arquivos de projeto.

---
+++ contaBancaria/Program.cs
@@ -0,0 +1,73 @@
+﻿ContaBancaria novaConta = new(123, "Vitoria", "123", 0);
+Console.WriteLine($"Bem vindo (a){novaConta.NomeTitular}");
+Console.WriteLine("O que deseja fazer: \n");
+Console.WriteLine("1 - Ver saldo");
+Console.WriteLine("2 - Realizar saque");
+Console.WriteLine("3 - Realizar depósito");
+Console.WriteLine("4- Alterar senha");
+
+string escolha = Console.ReadLine()!;
+
+switch (escolha)
+{
+    case "1":
+        {
+            Console.WriteLine($"Saldo atual: {novaConta.SaldoConta}");
+            break;
+        }
+    case "2":
+        {
+            Console.WriteLine("Quanto deseja retirar?");
+            double saque = double.Parse(Console.ReadLine()!);
+            novaConta.RetirarSaldo(saque);
+            break;
+        }
+    case "3":
+        {
+            Console.WriteLine("Quanto deseja adicionar?");
+            double valorAdicional = double.Parse(Console.ReadLine()!);
+            novaConta.AdicionarSaldo(valorAdicional);
+            break;
+        }
+    case "4":
+        {
+            while (true)
+            {
+                Console.WriteLine("\nDigite a senha atual:");
+                string senhaAtual = Console.ReadLine()!;
+                if (senhaAtual != novaConta.SenhaConta)
+                {
+                    Console.WriteLine("\nSenha inválida!");
+                    continue;
+                }
+                else
+                {
+
+                    while (true)
+                    {
+                        Console.WriteLine("\nDigite a nova senha");
+                        string novaSenha = Console.ReadLine()!;
+                        novaConta.AlterarSenha(novaSenha);
+                        if (novaConta.AlterarSenha(novaSenha) == false)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Senha alterada com sucesso!");
+                            break;
+                        }
+                    }
+                    
+                }
+                break;
+            }
+            break;
+        }
+        default:
+        {
+            Console.WriteLine("Opção inválida");
+            break;
+        }
+
+}
---
+++ contaBancaria/classContaBancaria.cs
@@ -0,0 +1,69 @@
+﻿class ContaBancaria
+{
+    public ContaBancaria(int numeroConta, string nomeTitular, string senhaConta, double saldo)
+    {
+        NumeroConta = numeroConta;
+        NomeTitular = nomeTitular;
+        SenhaConta = senhaConta;
+        SaldoConta = saldo;
+    }
+
+    public int NumeroConta { get; }
+    public string NomeTitular { get; }
+    public string SenhaConta { get; }
+    public double SaldoConta { get; set; }
+
+    public bool AlterarSenha(string senhaNova)
+    {
+
+        if (senhaNova == SenhaConta)
+        {
+            Console.WriteLine("Por favor, informe uma senha diferente da anterior");
+            return false;
+        }
+        else
+        {
+            if (senhaNova.Length > 0)
+            {
+                senhaNova = SenhaConta;
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Informe uma quantidade maior que 0");
+                return false;
+
+            }
+        }
+    }
+
+    public void AdicionarSaldo(double novoValor)
+    {
+        if (novoValor <= 0)
+        {
+            Console.WriteLine("Informe um valor maior que zero!");
+        }
+        else
+        {
+            SaldoConta += novoValor;
+            Console.WriteLine("Valor adicionado a sua carteira!\n");
+            Console.WriteLine($"Novo saldo: {SaldoConta}");
+        }
+    }
+    public void RetirarSaldo(double valorRetirar)
+    {
+        if (valorRetirar <= 0)
+        {
+            Console.WriteLine("Digite um valor de saque válido.");
+        }
+        else if (valorRetirar > SaldoConta)
+        {
+            Console.WriteLine("Valor maior do que o saldo em conta.");
+        }
+        else
+        {
+            SaldoConta -= valorRetirar;
+            Console.WriteLine("Saque realizado com sucesso!");
+        }
+    }
+}
